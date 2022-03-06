f836b8f253d2676bc9d3ff944a6271b8172007ac
Nathan Watkins <nathancwatkins23@gmail.com>
2022-03-06 11:05:55 -0800

    Exercise 5.7 completed: blogDetails component with visibility state

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,41 @@
-function Blog({ blog }) {
+import { useState } from 'react'
+
+function BlogDetails({ blog, visible }) {
+    if (!visible) return null
     return (
         <div>
-            {blog.title} {blog.author}
+            <p>by: {blog.author}</p>
+            <a href="blog.url">{blog.url} </a>
+            <p>
+                likes:{blog.likes}
+                <button type="button">like</button>
+            </p>
+        </div>
+    )
+}
+
+function Blog({ blog }) {
+    const [visible, setVisible] = useState(false)
+
+    const toggleVisibility = () => {
+        setVisible(!visible)
+    }
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        marginBottom: 5,
+    }
+    const viewBtnStyle = {
+        margin: 5,
+    }
+    return (
+        <div style={blogStyle}>
+            {blog.title}
+            <button type="button" style={viewBtnStyle} onClick={toggleVisibility}>
+                {visible ? 'hide' : 'view'}
+            </button>
+            <BlogDetails blog={blog} visible={visible} />
         </div>
     )
 }
